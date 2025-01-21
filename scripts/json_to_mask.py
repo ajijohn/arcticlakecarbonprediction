1edeff70ec990fcece40f7f99476de7788d5157f
refact0r <34758569+refact0r@users.noreply.github.com>
2025-01-21 15:37:56 -0800

    adjustments + new script

--- scripts/json_to_mask.py
+++ scripts/json_to_mask.py
@@ -4,31 +4,32 @@ import numpy as np
 import cv2
 from pathlib import Path
 
-images_dir = Path("../newdata/images")
-json_dir = Path("../newdata/json")
-masks_dir = Path("../newdata/masks")
+root_dir = Path("../newdata/json")
+output_dir = Path("../newdata/masks")
 
 
-def gen_mask_img(image_filename):
-    json_filename = json_dir / f"{image_filename.stem}.json"
-    image = cv2.imread(str(image_filename))
+def gen_mask_img(json_filename):
+    # read json file
+    with open(json_filename, "r") as f:
+        data = json.load(f)
+
+    data["imagePath"] = data["imagePath"].replace("\\", "/")
+    original_img_filename = json_filename.parent / data["imagePath"]
+
+    # read image to get shape
+    image = cv2.imread(str(original_img_filename))
 
     # create a blank image
     mask = np.zeros_like(image, dtype=np.uint8)
 
-    if json_filename.exists():
-        # read json file
-        with open(json_filename, "r") as f:
-            data = json.load(f)
+    # iterate over all shapes and draw them on the mask
+    for shape in data["shapes"]:
+        points = np.array(
+            shape["points"], dtype=np.int32
+        )  # tips: points location must be int32
+        cv2.fillPoly(mask, [points], (255, 255, 255))
 
-        # iterate over all shapes and draw them on the mask
-        for shape in data["shapes"]:
-            points = np.array(
-                shape["points"], dtype=np.int32
-            )  # tips: points location must be int32
-            cv2.fillPoly(mask, [points], (255, 255, 255))
-
-    mask_img_filename = masks_dir / f"{image_filename.stem}.jpg"
+    mask_img_filename = output_dir / f"{json_filename.stem}.jpg"
 
     # save the mask with the highest quality
     cv2.imwrite(str(mask_img_filename), mask, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
@@ -37,9 +38,9 @@ def gen_mask_img(image_filename):
 
 
 def main():
-    masks_dir.mkdir(parents=True, exist_ok=True)
-    for image_file in images_dir.rglob("*.*"):
-        print(f"{image_file} -> {gen_mask_img(image_file)}")
+    output_dir.mkdir(parents=True, exist_ok=True)
+    for json_file in root_dir.rglob("*.json"):
+        print(f"{json_file} -> {gen_mask_img(json_file)}")
 
 
 if __name__ == "__main__":
---
+++ scripts/json_to_mask_all.py
@@ -0,0 +1,46 @@
+import os
+import json
+import numpy as np
+import cv2
+from pathlib import Path
+
+images_dir = Path("../newdata/images")
+json_dir = Path("../newdata/json")
+masks_dir = Path("../newdata/masks")
+
+
+def gen_mask_img(image_filename):
+    json_filename = json_dir / f"{image_filename.stem}.json"
+    image = cv2.imread(str(image_filename))
+
+    # create a blank image
+    mask = np.zeros_like(image, dtype=np.uint8)
+
+    if json_filename.exists():
+        # read json file
+        with open(json_filename, "r") as f:
+            data = json.load(f)
+
+        # iterate over all shapes and draw them on the mask
+        for shape in data["shapes"]:
+            points = np.array(
+                shape["points"], dtype=np.int32
+            )  # tips: points location must be int32
+            cv2.fillPoly(mask, [points], (255, 255, 255))
+
+    mask_img_filename = masks_dir / f"{image_filename.stem}.jpg"
+
+    # save the mask with the highest quality
+    cv2.imwrite(str(mask_img_filename), mask, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+
+    return mask_img_filename
+
+
+def main():
+    masks_dir.mkdir(parents=True, exist_ok=True)
+    for image_file in images_dir.rglob("*.*"):
+        print(f"{image_file} -> {gen_mask_img(image_file)}")
+
+
+if __name__ == "__main__":
+    main()
